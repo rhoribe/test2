935a0d30ca736af165f2246acf0cc2eee986aaf3
rhoribe <rhoribe1908@gmail.com>
2023-11-15 21:38:52 -0300

    test

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "example" {
+  bucket = "rhoribe-test-bucket222"
+
+  tags = {
+    Name        = "rhoribe-test-bucket222"
+    Environment = "Dev"
+  }
+}
+
+
+
+terraform {
+  backend "s3" {
+    bucket         = var.bucket
+    key            = var.key
+    region         = var.region
+    encrypt        = true
+  }
+}
+
+
